b79498f7c4d7144858a34fc6d50410fbed097fbd
Lewis Britton <lewisbritton23@yahoo.co.uk>
2023-01-30 23:26:25 +0000

    Initiate 2

--- src/index.js
+++ src/index.js
@@ -1,17 +1,68 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import { BrowserRouter, BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import Header from './elements/page/Header';
+import Footer from './elements/page/Footer';
+
+import ErrorPage from './pages/ErrorPage';
+
+import Home from './pages/Home';
+import Academia from './pages/Academia';
+import Employment from './pages/Employment';
+import Library from './pages/Library';
+
+import InstantGram from './pages/blog/InstantGram';
+import Sport from './pages/blog/Sport';
+import Allroad from './pages/blog/Allroad';
+import BrittonsFoodRC from './pages/blog/BrittonsFoodRC';
+import FoundingFathers from './pages/blog/FoundingFathers';
+import Media from './pages/blog/Media';
+import MiamiVice from './pages/blog/MiamiVice';
+import Ricing from './pages/blog/Ricing';
+import ThinkFlow from './pages/blog/ThinkFlow';
+import GentlemenWhoCafe from './pages/blog/GentlemenWhoCafe';
+import GraphicDesign from './pages/blog/GraphicDesign';
+
+import Weather from './pages/utils/Weather.tsx';
+import Conquest from './pages/utils/Conquest';
+
+const root = ReactDOM.createRoot(
+  document.getElementById('root')
+);
+
 root.render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/academia" element={<Academia />} />
+        <Route path="/employment" element={<Employment />} />
+        <Route path="/library" element={<Library />} />
+        <Route path="/blog">
+          <Route path="instant-gram" element={<InstantGram />} />
+          <Route path="sport" element={<Sport />} />
+          <Route path="brittons-foodrc" element={<BrittonsFoodRC />} />
+          <Route path="allroad" element={<Allroad />} />
+          <Route path="gentlemen-who-cafe" element={<GentlemenWhoCafe />} />
+          <Route path="ricing" element={<Ricing />} />
+          <Route path="think-flow" element={<ThinkFlow />} />
+          <Route path="founding-fathers" element={<FoundingFathers />} />
+          <Route path="media" element={<Media />} />
+          <Route path="miami-vice" element={<MiamiVice />} />
+          <Route path="graphic-design" element={<GraphicDesign />} />
+        </Route>
+        <Route path="/utils">
+          <Route path="weather" element={<Weather />} />
+          <Route path="conquest" element={<Conquest />} />
+        </Route>
+        <Route path="*" element={<ErrorPage />} />
+      </Routes>
+      <Footer />
+    </BrowserRouter>
   </React.StrictMode>
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
 reportWebVitals();
