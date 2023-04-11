1cea256eb49e80f5b382aa4e5d6125bca54a79c5
Lewis Britton <lewisbritton23@yahoo.co.uk>
2023-04-11 21:26:36 +0100

    ..

---
+++ src/data/Events.js
@@ -0,0 +1,260 @@
+export const eventData = {
+	"2023": [
+		{
+			"id": "E2023001",
+			"namePrefix": "",
+			"nameSuffix": "",
+			"startDate": "",
+			"endDate": "",
+			"description": "",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+	],
+	"2022": [
+		{
+			"id": "E2022001",
+			"namePrefix": "",
+			"nameSuffix": "",
+			"startDate": "",
+			"endDate": "",
+			"description": "",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+	],
+	"2021": [
+		{
+			"id": "E2021001",
+			"namePrefix": "",
+			"nameSuffix": "",
+			"startDate": "",
+			"endDate": "",
+			"description": "",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+	],
+	"2020": [
+		{
+			"id": "E2020011",
+			"namePrefix": "",
+			"nameSuffix": "Some Stuff",
+			"startDate": "December",
+			"endDate": "",
+			"description": "Some normie happenings after the first project season came to an end.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020010",
+			"namePrefix": "",
+			"nameSuffix": "Cairnsmore of Carsphairn",
+			"startDate": "19th of December",
+			"endDate": "",
+			"description": "Another regret-filled project in the old Timbys. Struggled to move pretty much all day to be honest. Good stop in Ayr afterwards though at M&S for some late lunch and mince pies. Also the historic day when Griff had to wear a skull China-19 mask coz it’s all Poundland had.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [
+				"cairnsmore",
+			],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020009",
+			"namePrefix": "Ben Oolium:",
+			"nameSuffix": "Ultimate Supreme Depression",
+			"startDate": "14th of November",
+			"endDate": "",
+			"description": "Yeah this was probably the most depressing day I’ve had in the Scottish outdoors so far. Starting the day with a flat tyre should clearly have been a sign of caution. The hypothermia, white-out and trench foot did however, make for the most rewarding Green Welly Stop coffee ever. But of course the night had to be capped off by me dropping my crate.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [
+				"chullum",
+			],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020008",
+			"namePrefix": "Cairngorms:",
+			"nameSuffix": "An Aviemore Virgin",
+			"startDate": "11th of September",
+			"endDate": "13th of September",
+			"description": "My first experience of the Cairngorms since 2011. My first ever experience of the fine establishment which is the Pine Marten Baaar. This was an excellent weekend however, as usual, my choice of the Loake walking attire wasn’t a smart move. Left me with a buckled knee and a goddamn damaged something else which took about three months to heal. The ‘las in the Glenmore Youth Hostel made up for it though. Even if I couldn’t do Carn Gorm the next day.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [
+				"anAviemoreVirgin",
+			],
+			"youthHostels": [
+				"Cairngorm Lodge (Glenmore)",
+			],
+		},
+		{
+			"id": "E2020007",
+			"namePrefix": "",
+			"nameSuffix": "Bag One Get Three Free",
+			"startDate": "5th of September",
+			"endDate": "",
+			"description": "First multi-‘ro project. It’s probably one of the easiest projects in the Scottish Highlands but, once again, my choice of Timberland Pradas did not make the route the first bit enjoyable. Grow up Britton, get your act together!",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [
+				"bagOneGetThreeFree",
+			],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020006",
+			"namePrefix": "Fort William & Mallaig:",
+			"nameSuffix": "Actually Arisaig",
+			"startDate": "14th of August",
+			"endDate": "15th of August",
+			"description": "Started off as a very promising weekend. M&S BBQ in Arisaig along with a crate. The next day on Ben Nevis should have made it into the project library however, the wind farm lap sun burn from the previous Thursday left me crippled. Wore my fleece to cover my arms and neck on a 37° day.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020005",
+			"namePrefix": "",
+			"nameSuffix": "Ben Lomond",
+			"startDate": "3rd of August",
+			"endDate": "",
+			"description": "My, unfortunately and very unfittingly, soy introduction to the project game.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [
+				"benLomond",
+			],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020004",
+			"namePrefix": "",
+			"nameSuffix": "Some Stuff",
+			"startDate": "April",
+			"endDate": "July",
+			"description": "Some craic from the summer of the shit show and pretty much changing personality before changing personality again in 2021. Summer 2020 was excellent, don’t get me wrong. A lot of new developments I’m very thankful for. Too personal for here, soz fans.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020003",
+			"namePrefix": "Gentlemen Who Café Present:",
+			"nameSuffix": "Glasgow Adventures",
+			"startDate": "13th of February",
+			"endDate": "",
+			"description": "Some adventures of the Gentlemen Who Café duo around Glesgee in the final days of in-person uni.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020002",
+			"namePrefix": "Gentlemen Who Café Present:",
+			"nameSuffix": "Cheshire 2020 – The Golden Triangle",
+			"startDate": "24th of January",
+			"endDate": "26th of January",
+			"description": "Finally, the Gentlemen Who Café take to the streets of Wilmslow, Knutsford and Hale. A very interesting weekend of an unexpected Rosey Nosey successful wasted, ThinkPad development, horrific restaurants and boring low-level walks. Worth it tho.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+		{
+			"id": "E2020001",
+			"namePrefix": "",
+			"nameSuffix": "Loch Lomond",
+			"startDate": "12th of January",
+			"endDate": "",
+			"description": "The standard winter stroll around Loch Lomond with the Mancunians. Almost like the calm before the shit show.",
+			"images": [
+				{
+					"url": "",
+					"alt": "",
+					"description": ""
+				},
+			],
+			"routeNames": [],
+			"youthHostels": [],
+		},
+	]
+}
