a74a13c7f797a5a8fa5bd55e431fcbc58d1d657f
Lewis Britton <lewisbritton23@yahoo.co.uk>
2023-05-07 09:34:37 +0100

    deploy test update

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {
-  BrowserRouter,
-  BrowserRouter as Router,
+  HashRouter,
+  // BrowserRouter,
+  // BrowserRouter as Router,
   Route,
   Routes
 } from "react-router-dom";
@@ -39,37 +40,37 @@ const root = ReactDOM.createRoot(
 );
 
 root.render(
-  <React.StrictMode>
-    <BrowserRouter basename='/brittonsbash'>
-      <Header />
-      <Routes>
-        <Route path="/brittonsbash" element={<Home />} />
-        <Route path="/academia" element={<Academia />} />
-        <Route path="/employment" element={<Employment />} />
-        <Route path="/library" element={<Library />} />
-        <Route path="/blog">
-          <Route path="instant-gram" element={<InstantGram />} />
-          <Route path="sport" element={<Sport />} />
-          <Route path="brittons-foodrc" element={<BrittonsFoodRC />} />
-          <Route path="allroad" element={<Allroad />} />
-          <Route path="gentlemen-who-cafe" element={<GentlemenWhoCafe />} />
-          <Route path="ricing" element={<Ricing />} />
-          <Route path="think-flow" element={<ThinkFlow />} />
-          <Route path="founding-fathers" element={<FoundingFathers />} />
-          <Route path="media" element={<Media />} />
-          <Route path="miami-vice" element={<MiamiVice />} />
-          <Route path="graphic-design" element={<GraphicDesign />} />
-        </Route>
-        <Route path="/utils">
-          <Route path="weather" element={<Weather />} />
-          <Route path="conquest" element={<Conquest />} />
-          <Route path="bash-blocks" element={<BashBlocks />} />
-        </Route>
-        <Route path="*" element={<ErrorPage />} />
-      </Routes>
-      <Footer />
-    </BrowserRouter>
-  </React.StrictMode>
+  // <React.StrictMode>
+  <HashRouter>
+    <Header />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/academia" element={<Academia />} />
+      <Route path="/employment" element={<Employment />} />
+      <Route path="/library" element={<Library />} />
+      <Route path="/blog">
+        <Route path="instant-gram" element={<InstantGram />} />
+        <Route path="sport" element={<Sport />} />
+        <Route path="brittons-foodrc" element={<BrittonsFoodRC />} />
+        <Route path="allroad" element={<Allroad />} />
+        <Route path="gentlemen-who-cafe" element={<GentlemenWhoCafe />} />
+        <Route path="ricing" element={<Ricing />} />
+        <Route path="think-flow" element={<ThinkFlow />} />
+        <Route path="founding-fathers" element={<FoundingFathers />} />
+        <Route path="media" element={<Media />} />
+        <Route path="miami-vice" element={<MiamiVice />} />
+        <Route path="graphic-design" element={<GraphicDesign />} />
+      </Route>
+      <Route path="/utils">
+        <Route path="weather" element={<Weather />} />
+        <Route path="conquest" element={<Conquest />} />
+        <Route path="bash-blocks" element={<BashBlocks />} />
+      </Route>
+      <Route path="*" element={<ErrorPage />} />
+    </Routes>
+    <Footer />
+  </HashRouter>
+  // </React.StrictMode>
 );
 
 reportWebVitals();
