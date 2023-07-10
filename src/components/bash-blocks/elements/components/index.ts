c68bc20e7e3e3146492e50302e247ca44c326ee4
U356511 <l.britton@scottishpower.com>
2023-07-10 17:03:36 +0100

    wip

--- src/components/bash-blocks/elements/components/index.ts
+++ src/components/bash-blocks/elements/components/index.ts
@@ -1,6 +1,7 @@
 export * from './button.component'
 export * from './figure.component'
 export * from './flex.component'
+export * from './image-matrix.component'
 export * from './image.component'
 export * from './search.component'
 export * from './search-list.component'
