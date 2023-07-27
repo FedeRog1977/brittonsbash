dcca00435accca356ef805e93d82080b840fa142
U356511 <l.britton@scottishpower.com>
2023-07-27 11:33:28 +0100

    remove drop-down

--- src/components/compounds/components/weather-daily.component.tsx
+++ src/components/compounds/components/weather-daily.component.tsx
@@ -15,36 +15,30 @@ export const WeatherDaily = ({ latIn, lonIn }: any) => {
     return (
         <>
             <Flex>
-                {dailyResult.map((value, idx) => {
-                    {
-                        if (idx <= 6) {
-                            return (
-                                <Flex item>
-                                    <WeatherColumnDaily
-                                        key={idx}
-                                        dt={value.dt}
-                                        icon={value.weather[0].icon}
-                                        temp={value.temp.day}
-                                        pop={value.pop}
-                                        desc={value.weather[0].description}
-                                        sr={value.sunrise}
-                                        ss={value.sunset}
-                                        tempMax={value.temp.max}
-                                        tempMin={value.temp.min}
-                                        tempFlDay={value.feels_like.day}
-                                        tempFlNight={value.feels_like.night}
-                                        windDeg={value.wind_deg}
-                                        windSpd={value.wind_speed}
-                                        pressure={value.pressure}
-                                        humidity={value.humidity}
-                                        dp={value.dew_point}
-                                        uvi={value.uvi}
-                                    />
-                                </Flex>
-                            )
-                        }
-                    }
-                })}
+                {dailyResult.slice(0, 7).map((value, idx) => (
+                    <Flex item>
+                        <WeatherColumnDaily
+                            key={idx}
+                            dt={value.dt}
+                            icon={value.weather[0].icon}
+                            temp={value.temp.day}
+                            pop={value.pop}
+                            desc={value.weather[0].description}
+                            sr={value.sunrise}
+                            ss={value.sunset}
+                            tempMax={value.temp.max}
+                            tempMin={value.temp.min}
+                            tempFlDay={value.feels_like.day}
+                            tempFlNight={value.feels_like.night}
+                            windDeg={value.wind_deg}
+                            windSpd={value.wind_speed}
+                            pressure={value.pressure}
+                            humidity={value.humidity}
+                            dp={value.dew_point}
+                            uvi={value.uvi}
+                        />
+                    </Flex>
+                ))}
             </Flex>
             <Spacing mT={isMobile ? 7.5 : 15} mB={isMobile ? 7.5 : 15}>
                 <Button
--- src/components/compounds/components/weather-hourly.component.tsx
+++ src/components/compounds/components/weather-hourly.component.tsx
@@ -8,32 +8,26 @@ export const WeatherHourly = ({ latIn, lonIn }: any) => {
 
     return (
         <Flex>
-            {hourlyResult.map((value, idx) => {
-                {
-                    if (idx <= 7) {
-                        return (
-                            <Flex item>
-                                <WeatherColumnHourly
-                                    key={idx}
-                                    dt={value.dt}
-                                    icon={value.weather[0].icon}
-                                    temp={value.temp}
-                                    tempFl={value.feels_like}
-                                    pop={value.pop}
-                                    windDeg={value.wind_deg}
-                                    windSpd={value.wind_speed}
-                                    windGst={value.wind_gust}
-                                    pressure={value.pressure}
-                                    humidity={value.humidity}
-                                    dp={value.dew_point}
-                                    vb={value.visibility}
-                                    uvi={value.uvi}
-                                />
-                            </Flex>
-                        )
-                    }
-                }
-            })}
+            {hourlyResult.slice(0, 7).map((value, idx) => (
+                <Flex item>
+                    <WeatherColumnHourly
+                        key={idx}
+                        dt={value.dt}
+                        icon={value.weather[0].icon}
+                        temp={value.temp}
+                        tempFl={value.feels_like}
+                        pop={value.pop}
+                        windDeg={value.wind_deg}
+                        windSpd={value.wind_speed}
+                        windGst={value.wind_gust}
+                        pressure={value.pressure}
+                        humidity={value.humidity}
+                        dp={value.dew_point}
+                        vb={value.visibility}
+                        uvi={value.uvi}
+                    />
+                </Flex>
+            ))}
         </Flex>
     )
 }
