528fe06c5f2a3fca32edbaf14f1513dd09e984d4
Lewis Britton <lewis-britton@HOR140158B.local>
2024-06-04 22:55:45 +0100

    wip

--- src/components/bash-blocks/basics/grid-temp/grid-item.tsx
+++ src/components/bash-blocks/basics/grid-temp/grid-item.tsx
@@ -1,11 +1,10 @@
 import cx from 'classnames';
 import { FC, ReactNode } from 'react';
 import styles from './grid-item.module.scss';
-import { ColumnSpan } from '../../reference';
+import { ColumnSpan, getResponsiveColumnSpanClassNames } from '../../reference';
 import { Order } from './types/order';
 import { getOrderClassNames } from './utils/get-order-classnames';
 import { getPositionClassName } from './utils/get-position-classnames';
-import { getWidthClassName } from './utils/get-width-classnames';
 
 export type GridItemProps = {
   children: ReactNode;
@@ -32,14 +31,16 @@ export const GridItemTemp: FC<GridItemProps> = ({
   position,
 }) => {
   const classNames = cx(
-    getWidthClassName(styles, 'xs', xs),
-    getWidthClassName(styles, 'sm', sm),
-    getWidthClassName(styles, 'md', md),
-    getWidthClassName(styles, 'lg', lg),
-    getWidthClassName(styles, 'xl', xl),
-    getWidthClassName(styles, '2xl', xxl),
-    getPositionClassName(styles, position),
-    ...getOrderClassNames(styles, order)
+    ...getResponsiveColumnSpanClassNames('width', styles, {
+      xs,
+      sm,
+      md,
+      lg,
+      xl,
+      '2xl': xxl,
+    }),
+    ...getOrderClassNames(styles, order),
+    getPositionClassName(styles, position)
   );
 
   return <div className={classNames}>{children}</div>;
--- src/components/bash-blocks/basics/grid-temp/types/width.ts
+++
@@ -1,1 +0,0 @@
-export type Width = 'xs' | 'sm' | 'md' | 'lg' | 'xl' | '2xl';
