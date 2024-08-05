701e71d3a0e826d283e183b06837150873bb2e0d
Lewis Britton <lewis-britton@HOR140158B.local>
2024-08-05 15:12:12 +0100

    wip

--- src/components/bash-blocks/basics/typography/utils/enrich-markdown.ts
+++ src/components/bash-blocks/basics/typography/utils/enrich-markdown.ts
@@ -1,14 +1,17 @@
 import { characterMap } from '../constants/character-map';
 
 export const enrichMarkdown = (content: string) => {
-  content = content.replace(/\_{([^::]+)}/g, '<sub>$1</sub>');
-  content = content.replace(/\^{([^::]+)}/g, '<sup>$1</sup>');
+  content = content
+    .replace(/_{([^::]+)}/g, '<sub>$1</sub>')
+    .replace(/\^{([^::]+)}/g, '<sup>$1</sup>');
 
   for (const character in characterMap) {
-    content = content.replace(
-      new RegExp(character, 'g'),
-      characterMap[character]
-    );
+    if (content.match(character)) {
+      content = content.replace(
+        new RegExp(character, 'u'),
+        characterMap[character]
+      );
+    }
   }
 
   return content;
