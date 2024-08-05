d1ddaf6be6230452da0af75e1aa4683f7c9e0200
Lewis Britton <lewis-britton@HOR140158B.local>
2024-08-05 23:08:22 +0100

    wip

--- src/components/bash-blocks/basics/typography/utils/enrich-markdown.ts
+++ src/components/bash-blocks/basics/typography/utils/enrich-markdown.ts
@@ -2,12 +2,19 @@
 
 export const enrichMarkdown = (content: string) => {
   content = content
-    .replace(/`(?!`)/g, '&lsquo;')
-    .replace(/``/g, '&ldquo;')
-    .replace(/(?<!')'/g, '&rsquo;')
-    .replace(/''/g, '&rdquo;')
+    // Desired but doesn't work:
+    // .replace(/([^::]+)'(?=^[A-Za-z]+$)/u, '$1&rsquo;')
+    // Replace a single-quotations
+    .replace(/(?<!`)`(?!`)([^::]+)(?<!')'(?!')/g, '&lsquo;$1&rsquo;')
+    // Replace all double-quotations
+    .replace(/``([^::]+)''/g, '&ldquo;$1&rdquo;')
+    // Replace remaining right-single-quotations (apostrophes)
+    .replace(/([^::]+)'/g, '$1&rsquo;')
+    // Replace dashes
     .replace(/-/g, '&mdash;')
+    // Replace subscript
     .replace(/_\{([^::]+)\}/g, '<sub>$1</sub>')
+    // Replace superscript
     .replace(/\^\{([^::]+)\}/g, '<sup>$1</sup>');
 
   // Unused as Record<> does not support RegExp :(
