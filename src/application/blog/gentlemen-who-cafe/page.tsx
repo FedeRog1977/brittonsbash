0bc36919e158aeeaccf12ba2b9eab5eff7076eac
Lewis Britton <lewisbritton23@yahoo.co.uk>
2024-11-13 22:15:02 +0000

    wip

--- src/application/blog/gentlemen-who-cafe/page.tsx
+++ src/application/blog/gentlemen-who-cafe/page.tsx
@@ -1,16 +1,9 @@
 import { ReactNode } from 'react';
-import { PageLayout, Tile } from '../../../components';
+import { ArticleTile, PageLayout } from '../../../components';
+import { gentlemenWhoCafeContent } from './content/gentlemen-who-cafe-content';
 
 export const GentlemenWhoCafe = () => {
-  const content: ReactNode[] = [
-    <Tile type="solid">
-      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut
-      labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco
-      laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in
-      voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-      non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-    </Tile>,
-  ];
+  const content: ReactNode[] = [<ArticleTile {...gentlemenWhoCafeContent.tileOne} />];
 
-  return <PageLayout background={{ content: 'Gentlemen Who Cafe' }}>{content}</PageLayout>;
+  return <PageLayout background={gentlemenWhoCafeContent.background}>{content}</PageLayout>;
 };
