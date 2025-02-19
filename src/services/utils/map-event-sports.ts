8758ae9141e0c7b57384bfa067b95d7fe8ebf13e
Lewis Britton <lewis-britton@HOR140158B.local>
2025-02-19 22:20:04 +0000

    Remove services and implementations

--- src/services/utils/map-event-sports.ts
+++
@@ -1,9 +0,0 @@
-import { Project, Sport } from '../../utils/types';
-
-export const mapEventSports = (sport: Sport): Project[] =>
-  sport[2025].projects
-    .concat(sport[2024].projects)
-    .concat(sport[2023].projects)
-    .concat(sport[2022].projects)
-    .concat(sport[2021].projects)
-    .concat(sport[2020].projects);
